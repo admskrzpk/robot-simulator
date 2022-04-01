f8ec51d1ab6eb9689b104a1b2ad921195c0ffda6
a.skrzypek <a.skrzypek@adform.com>
2022-04-01 17:43:50 +0200

    still having problems with types and pattern matching

--- src/main/scala/RobotSimulator.scala
+++ src/main/scala/RobotSimulator.scala
@@ -1,40 +1,33 @@
-import scala.collection.mutable.ArrayBuffer
 
 object RobotSimulator extends App {
-
   var x = args(1).toInt
   var y = args(2).toInt
   var orientation = args(3)
-  val commands = args(0).split("").toList
-  println(s"Robot is on $x and $y positions facing $orientation. Now robot is executing commands: $commands ")
+  val command = args(0).split("").toList
 
-  case class Robot(x: Int, y: Int, orientation: String)
-
-  val robot = Robot(x = args(1).toInt, y = args(2).toInt, orientation = args(3))
-
-  def turn(command: String): String = commands match {
-    case "L" if orientation == "north" => orientation = "east"
-    case "L" if orientation == "east" => orientation = "south"
-    case "L" if orientation == "east" => orientation = "north"
-    case "L" if orientation == "north" => orientation = "west"
-    case "R" if orientation == "north" => orientation = "east"
-    case "R" if orientation == "east" => orientation = "north"
-    case "R" if orientation == "south" => orientation = "west"
-    case "R" if orientation == "west" => orientation = "south"
+  def robotMoving(s: List[String]): Unit = {
+    s.foreach {
+      case "L" => orientation match {
+        case "north" => orientation = "west"
+        case "east" => orientation = "north"
+        case "south" => orientation = "west"
+        case "west" => orientation = "south"
+      }
+      case "R" => orientation match {
+        case "north" => orientation = "east"
+        case "east" => orientation = "south"
+        case "south" => orientation = "west"
+        case "west" => orientation = "north"
+      }
+      case "A" => orientation match {
+        case "north" => y = y + 1
+        case "east" => x = x + 1
+        case "south" => y = y - 1
+        case "west" => x = x - 1
+      }
+    }
+    println(s"Robots position is $x,$y facing $orientation")
   }
 
-  def advance(command: String): (Int, Int) = orientation match {
-    case "north" => (x, y + 1)
-    case "east" => (x + 1, y)
-    case "south" => (x, y - 1)
-    case "west" => (x - 1, y)
-  }
-
-  commands.foldLeft(robot)(commands match {
-    case "R" => turn(orientation)
-    case "L" => turn(orientation)
-    case "A" => advance(x, y)
-  })
-
-  println(s"Robots position is $x,$y facing $orientation?")
+  robotMoving(command)
 }
